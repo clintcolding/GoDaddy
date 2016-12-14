5fbbc80edb13296db9b8b4d7af994e095143377b
Clint Colding <clintcolding@gmail.com>
2016-12-14 11:53:51 -0500

    Initial commit

---
+++ Update-GoDaddyAPIKey.ps1
@@ -0,0 +1,54 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Update-GoDaddyAPIKey
+{
+    [CmdletBinding()]
+
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]$Key,
+
+        [Parameter(Mandatory=$true,
+                   Position=1)]
+        [string]$Secret
+    )
+
+    Begin
+    {
+        
+    }
+    Process
+    {
+        # Get and set module path
+        
+        $Path = Get-Module GoDaddyDNS | Select-Object Path
+        $Path = ($Path.Path).TrimEnd('GoDaddyDNS.psd1')
+        $Path = $Path + "Get-GoDaddyDNS.ps1"
+
+        # Get current key
+
+        [string]$String = Select-String -Path $Path -Pattern "Key="
+        $CurrentKey = ($String.Substring($String.IndexOf("'")+1)).TrimEnd(",","'")
+
+        # Replace current key with new key
+
+        $Updated = (Get-Content $Path).Replace($CurrentKey,$Key)
+
+        # Update function
+
+        Set-Content -Path $Path -Value $Updated
+    }
+    End
+    {
+    }
+}
