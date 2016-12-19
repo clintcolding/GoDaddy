b4fb941e867c94ebe656a7a8a7a10edf37d21b21
Clint Colding <clintcolding@gmail.com>
2016-12-19 10:42:55 -0500

    Reworked entire function

--- Get-GoDaddyAPIKey.ps1
+++ Get-GoDaddyAPIKey.ps1
@@ -6,10 +6,9 @@
 .EXAMPLE
    PS C:\> Get-GoDaddyAPIKey
 
-   Command        Key   Secret   Path                                        
-   -------        ---   ------   ----                                        
-   Get-GoDaddyDNS mykey mysecret C:\Users\ccolding\Documents\WindowsPowerShell\Modules\Get-GoDaddyDNS.ps1
-   Set-GoDaddyDNS mykey mysecret C:\Users\ccolding\Documents\WindowsPowerShell\Modules\Set-GoDaddyDNS.ps1
+   Key    Secret   
+   ---    ------   
+   mykey  mysecret
 #>
 function Get-GoDaddyAPIKey
 {
@@ -24,33 +23,7 @@ function Get-GoDaddyAPIKey
     }
     Process
     {       
-        # Get module path
-        
-        $Paths = ((Get-Module GoDaddy | select ModuleBase).ModuleBase | Get-ChildItem | Where-Object {$_.Name -like 'Get-GoDaddyDNS*' -or $_.Name -like 'Set-GoDaddyDNS*' -or $_.Name -like 'Find-*'} | Select-Object FullName).FullName
-
-        foreach ($Path in $Paths)
-        {
-            # Get current key
-            
-            [string]$KeyString = Select-String -Path $Path -Pattern "Key="
-            $CurrentKey = ($KeyString.Substring($KeyString.IndexOf("'")+1)).TrimEnd(",","'")
-
-            # Get current secret
-            
-            [string]$SecretString = Select-String -Path $Path -Pattern "Secret="
-            $CurrentSecret = ($SecretString.Substring($SecretString.IndexOf("'")+1)).TrimEnd("'")
-
-            $Prop = [ordered] @{
-                        Command = ($Path | Get-ChildItem -Name).TrimEnd(".ps1")
-                        Key     = $CurrentKey
-                        Secret  = $CurrentSecret
-                        Path    = $Path
-                    }
-            
-            $Obj=New-Object -TypeName PSObject -Property $Prop
-            
-            Write-Output $Obj
-        }
+        Import-Csv "$PSScriptRoot\apiKey.csv"
     }
     End
     {
