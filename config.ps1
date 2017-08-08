d3cf3f556a1aceda260a4c44dd7f95ecb2632115
Clint Colding <clintcolding@gmail.com>
2017-08-08 15:38:36 -0400

    Initial commit

---
+++ config.ps1
@@ -0,0 +1,14 @@
+$Provider = @{
+
+    Frontier = @{
+        Services = @{
+            test = '96.254.141.248'
+        }
+    }
+
+    Level3 = @{
+        Services = @{
+            test = '66.199.7.189'
+        }
+    }
+}
