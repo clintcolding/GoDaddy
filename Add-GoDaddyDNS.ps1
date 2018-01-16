d99720bc888d241e5e93b1a046426d524c29bbf4
Clint Colding <clintcolding@gmail.com>
2018-01-16 15:46:56 -0500

    Added dynamic param for SRV records

--- Add-GoDaddyDNS.ps1
+++ Add-GoDaddyDNS.ps1
@@ -38,21 +38,113 @@ function Add-GoDaddyDNS
         [int]$TTL=3600
     )
 
+    DynamicParam {
+        if ($Type -eq "SRV") {
+            # Inititalize runtime dictionary
+            $paramDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+            # Priority ParameterAttribute object
+            $priorityAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $priorityAttribute.Mandatory = $true
+            $priorityAttribute.HelpMessage = "Please enter record priority:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($priorityAttribute)
+
+            # Add paramater specifying the attribute collection
+            $priorityParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Priority', [int32], $attributeCollection)
+
+            # Service ParameterAttribute object
+            $serviceAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $serviceAttribute.Mandatory = $true
+            $serviceAttribute.HelpMessage = "Please enter SRV service:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($serviceAttribute)
+
+            # Add paramater specifying the attribute collection
+            $serviceParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Service', [string], $attributeCollection)
+
+            # Protocol ParameterAttribute object
+            $protocolAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $protocolAttribute.Mandatory = $true
+            $protocolAttribute.HelpMessage = "Please enter SRV protocol:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($protocolAttribute)
+
+            # Add paramater specifying the attribute collection
+            $protocolParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Protocol', [string], $attributeCollection)
+
+            # Port ParameterAttribute object
+            $portAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $portAttribute.Mandatory = $true
+            $portAttribute.HelpMessage = "Please enter SRV port:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($portAttribute)
+
+            # Add paramater specifying the attribute collection
+            $portParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Port', [int32], $attributeCollection)
+
+            # Weight ParameterAttribute object
+            $weightAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $weightAttribute.Mandatory = $true
+            $weightAttribute.HelpMessage = "Please enter SRV weight:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($weightAttribute)
+
+            # Add paramater specifying the attribute collection
+            $weightParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Weight', [int32], $attributeCollection)
+
+            #Add the names of our parameters
+            $paramDictionary.Add('Priority', $priorityParam)
+            $paramDictionary.Add('Service', $serviceParam)
+            $paramDictionary.Add('Protocol', $protocolParam)
+            $paramDictionary.Add('Port', $portParam)
+            $paramDictionary.Add('Weight', $weightParam)
+
+            return $paramDictionary
+        }
+    }
+
     Begin
     {
         $apiKey = Import-Csv "$PSScriptRoot\apiKey.csv"
     }
     Process
     {        
-        $Headers = @{}
-        $Headers["Authorization"] = 'sso-key ' + $apiKey.key + ':' + $apiKey.secret
-        $Headers["Content-Type"] = "application/json"
-        $Headers["Accept"] = "application/json"
+        $headers = @{}
+        $headers["Authorization"] = 'sso-key ' + $apiKey.key + ':' + $apiKey.secret
+        $headers["Content-Type"] = "application/json"
+        $headers["Accept"] = "application/json"
 
-        $Record = @{type="$Type";name="$Name";data="$Data";ttl=$TTL}
-        $Body = "[" + (ConvertTo-Json $Record) + "]"
+        if ($Type -eq "SRV") {
+            $record = @{type="$Type";name="$Name";data="$Data";ttl=$TTL;priority=$PSBoundParameters.Priority;service="{0}" -f $PSBoundParameters.Service;protocol="{0}" -f $PSBoundParameters.Protocol;port=$PSBoundParameters.Port;weight=$PSBoundParameters.Weight}
+            $body = "[" + (ConvertTo-Json $record) + "]"
+        }
+        else {
+            $record = @{type="$Type";name="$Name";data="$Data";ttl=$TTL}
+            $body = "[" + (ConvertTo-Json $record) + "]"
+        }
 
-        Invoke-WebRequest https://api.godaddy.com/v1/domains/$Domain/records -Method Patch -Headers $Headers -Body $Body -UseBasicParsing | ConvertFrom-Json
+        Invoke-WebRequest https://api.godaddy.com/v1/domains/$Domain/records -Method Patch -Headers $headers -Body $body -UseBasicParsing | ConvertFrom-Json
 
         Get-GoDaddyDNS -Domain $Domain -Type $Type -Name $Name
     }
--- Set-GoDaddyDNS.ps1
+++ Set-GoDaddyDNS.ps1
@@ -35,6 +35,92 @@ function Set-GoDaddyDNS
         [int]$TTL=3600
     )
 
+    DynamicParam {
+        if ($Type -eq "SRV") {
+            # Inititalize runtime dictionary
+            $paramDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+            # Priority ParameterAttribute object
+            $priorityAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $priorityAttribute.Mandatory = $true
+            $priorityAttribute.HelpMessage = "Please enter record priority:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($priorityAttribute)
+
+            # Add paramater specifying the attribute collection
+            $priorityParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Priority', [int32], $attributeCollection)
+
+            # Service ParameterAttribute object
+            $serviceAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $serviceAttribute.Mandatory = $true
+            $serviceAttribute.HelpMessage = "Please enter SRV service:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($serviceAttribute)
+
+            # Add paramater specifying the attribute collection
+            $serviceParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Service', [string], $attributeCollection)
+
+            # Protocol ParameterAttribute object
+            $protocolAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $protocolAttribute.Mandatory = $true
+            $protocolAttribute.HelpMessage = "Please enter SRV protocol:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($protocolAttribute)
+
+            # Add paramater specifying the attribute collection
+            $protocolParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Protocol', [string], $attributeCollection)
+
+            # Port ParameterAttribute object
+            $portAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $portAttribute.Mandatory = $true
+            $portAttribute.HelpMessage = "Please enter SRV port:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($portAttribute)
+
+            # Add paramater specifying the attribute collection
+            $portParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Port', [int32], $attributeCollection)
+
+            # Weight ParameterAttribute object
+            $weightAttribute = New-Object System.Management.Automation.ParameterAttribute
+            $weightAttribute.Mandatory = $true
+            $weightAttribute.HelpMessage = "Please enter SRV weight:"
+
+            # AttributeCollection object for the above attribute
+            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+            # Add attribute to collection
+            $attributeCollection.Add($weightAttribute)
+
+            # Add paramater specifying the attribute collection
+            $weightParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Weight', [int32], $attributeCollection)
+
+            #Add the names of our parameters
+            $paramDictionary.Add('Priority', $priorityParam)
+            $paramDictionary.Add('Service', $serviceParam)
+            $paramDictionary.Add('Protocol', $protocolParam)
+            $paramDictionary.Add('Port', $portParam)
+            $paramDictionary.Add('Weight', $weightParam)
+
+            return $paramDictionary
+        }
+    }
+
     Begin
     {
         $apiKey = Import-Csv "$PSScriptRoot\apiKey.csv"
@@ -43,11 +129,19 @@ function Set-GoDaddyDNS
     {        
         $Headers = @{}
         $Headers["Authorization"] = 'sso-key ' + $apiKey.key + ':' + $apiKey.secret
+        $headers["Content-Type"] = "application/json"
+        $headers["Accept"] = "application/json"
 
-        $Record = @{data="$Data";ttl=$TTL}
-        $Body = ConvertTo-Json $Record
+        if ($Type -eq "SRV") {
+            $record = @{data="$Data";ttl=$TTL;priority=$PSBoundParameters.Priority;service="{0}" -f $PSBoundParameters.Service;protocol="{0}" -f $PSBoundParameters.Protocol;port=$PSBoundParameters.Port;weight=$PSBoundParameters.Weight}
+            $body = "[" + (ConvertTo-Json $record) + "]"
+        }
+        else {
+            $record = @{data="$Data";ttl=$TTL}
+            $body = "[" + (ConvertTo-Json $record) + "]"
+        }
 
-        Invoke-WebRequest https://api.godaddy.com/v1/domains/$Domain/records/$Type/$Name -Method Put -Headers $Headers -Body $Body -ContentType "application/json" -UseBasicParsing | Out-Null
+        Invoke-WebRequest https://api.godaddy.com/v1/domains/$Domain/records/$Type/$Name -Method Put -Headers $headers -Body $body -UseBasicParsing | Out-Null
 
         Get-GoDaddyDNS -Domain $Domain -Type $Type -Name $Name
     }
